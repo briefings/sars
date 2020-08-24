9758824ec0cea552cfb83e758d88209e02f22c8f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-08-24 23:00:35 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 cp fundamentals/atlantic/warehouse/candle/states/*.json graphs/spreads/data/atlantic
+
+cp fundamentals/atlantic/warehouse/gazetteer.csv graphs/statistics/atlantic
+cp fundamentals/atlantic/warehouse/increases.csv graphs/statistics/atlantic
