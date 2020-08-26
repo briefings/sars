2fdb9955691c99ae14d2dff3502cbb42ffb1cf39
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-08-26 16:10:41 +0100

    Update fundamentals/populations/populations/us/reference/settings.py

--- fundamentals/populations/populations/us/reference/settings.py
+++ fundamentals/populations/populations/us/reference/settings.py
@@ -3,5 +3,8 @@ class Settings:
 
     def __init__(self):
 
+        self.apicounty = 'https://raw.githubusercontent.com/discourses/hub/develop/' \
+                         'data/countries/us/population/api/counties.csv'
+
         self.apistate = 'https://www2.census.gov/programs-surveys/popest/datasets/{segment}/' \
                         'state/detail/SCPRC-EST{year}-18+POP-RES.csv'
