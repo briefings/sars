2a62f076367e025c8a035a4d22d850440e9f54aa
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-08-28 22:36:01 +0100

    Add fundamentals/hopkins/config.py: For fundamentals/hopkins/

---
+++ fundamentals/hopkins/config.py
@@ -0,0 +1,64 @@
+import os
+import platform
+from datetime import datetime
+from datetime import timedelta
+
+import numpy as np
+import pandas as pd
+
+import hopkins.base.directories
+
+
+class Config:
+
+    def __init__(self):
+
+        # Records began ...
+        self.starting: str = '2020-01-22'
+        self.epochdays: int = int(datetime.strptime(self.starting, '%Y-%m-%d').timestamp() / (60 * 60 * 24))
+
+        # End point
+        limit: datetime = datetime.today() - timedelta(days=1)
+        self.ending: str = limit.strftime('%Y-%m-%d')
+
+        # Source: The name of the J.H. date field, and the pattern of the dates
+        self.datestring = 'date'
+
+        if platform.system() == 'Windows':
+            self.datepattern = '%#m/%#d/%y'
+        else:
+            self.datepattern = '%-m/%-d/%y'
+
+        # The starting date field
+        self.startingfield: str = datetime.strptime(self.starting, '%Y-%m-%d').strftime(self.datepattern)
+
+        # Data
+        self.urlfields = 'https://raw.githubusercontent.com/premodelling/dictionaries/develop/' \
+                         'sars/johnHopkinsCountiesReference.json'
+
+        self.url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' \
+                   'csse_covid_19_time_series/time_series_covid19_{category}_US.csv'
+        self.categories = ['confirmed', 'deaths']
+        self.measures = {'confirmed': 'positiveCumulative', 'deaths': 'deathCumulative'}
+
+        # Results directory
+        self.warehouse = os.path.join(os.getcwd(), 'warehouse')
+
+    def storage(self):
+
+        directories = hopkins.base.directories.Directories()
+        directories.cleanup(listof=[self.warehouse])
+        directories.create(listof=[self.warehouse])
+
+    def days(self):
+        """
+        values.datetimeobject.apply(lambda x: x.value / (10 ** 6)).astype(np.longlong)
+
+        :return: A DataFrame of dates
+        """
+
+        values = pd.DataFrame(pd.date_range(start=self.starting, end=self.ending, freq='D'), columns=['datetimeobject'])
+        values.loc[:, self.datestring] = values.datetimeobject.apply(lambda x: x.strftime(self.datepattern))
+        values.loc[:, 'epochmilli'] = (values['datetimeobject'].astype(np.int64) / (10 ** 6)).astype(np.longlong)
+
+        return values
