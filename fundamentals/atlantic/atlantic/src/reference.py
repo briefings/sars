40a524f9648976f87bd42f5182ea853ea4a17dcf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-08-31 22:45:32 +0100

    Update fundamentals/atlantic/atlantic/src/reference.py: Cleaning-up

--- fundamentals/atlantic/atlantic/src/reference.py
+++ fundamentals/atlantic/atlantic/src/reference.py
@@ -1,3 +1,5 @@
+import logging
+
 import dask
 import pandas as pd
 
@@ -5,17 +7,26 @@ import pandas as pd
 class Reference:
 
     def __init__(self, dates: pd.DataFrame):
+
         self.dates = dates
 
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+
     def baselines(self, stusps: str):
+
         values = self.dates.copy()
         values.loc[:, 'STUSPS'] = stusps
 
         return values
 
     def exc(self, states: pd.DataFrame):
+
         computations = [dask.delayed(self.baselines)(stusps) for stusps in states.STUSPS.values]
         dask.visualize(computations, filename='reference', format='pdf')
-        values = dask.compute(computations, scheduler='processes')[0]
+        calculations = dask.compute(computations, scheduler='processes')[0]
+        values = pd.concat(calculations)
 
-        return pd.concat(values)
+        self.logger.info('\n{}\n'.format(values.tail()))
+
+        return values
