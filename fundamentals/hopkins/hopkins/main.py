56db57d79dc7b6df6aa14fc1facf942ea03377d1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-08-31 15:59:17 +0100

    Update fundamentals/hopkins/hopkins/main.py: In progress

--- fundamentals/hopkins/hopkins/main.py
+++ fundamentals/hopkins/hopkins/main.py
@@ -33,29 +33,53 @@ def main():
     features = hopkins.src.features.Features(datestrings=days[configurations.datestring].values,
                                              basefields=['FIPS']).exc()
 
-    # Reference
+    # Reference: Creates all distinct county & dates combinations per county
     reference = hopkins.src.reference.Reference(days=days,
-                                                gazetter=gazetteer[['STATEFP', 'COUNTYGEOID', inhabitants]]
+                                                gazetter=gazetteer[['STATEFP', 'STUSPS', 'COUNTYGEOID', inhabitants]]
                                                 ).exc()
 
-    # Readings
+    # Readings: Reads and structures
     readings = hopkins.src.readings.Readings(features=features, reference=reference, days=days).exc()
 
-    # Addressing Anomalies
-    hencecounty = hopkins.algorithms.anomalies.Anomalies(blob=readings).exc()
+    # Addressing Anomalies. Therefore, county & state readings after corrections ...
+    rvc = hopkins.algorithms.anomalies.Anomalies(blob=readings).exc()
+    logger.info('\n{}\n'.format(rvc.info()))
 
-    hencestate: pd.DataFrame = hencecounty.drop(columns=['COUNTYGEOID']).\
-        groupby(by=['datetimeobject', 'date', 'epochmilli', 'STATEFP']).sum()
-    hencestate.reset_index(drop=False, inplace=True)
+    rvs: pd.DataFrame = rvc.drop(columns=['COUNTYGEOID']). \
+        groupby(by=['datetimeobject', 'date', 'epochmilli', 'STATEFP', 'STUSPS']).sum()
+    rvs.reset_index(drop=False, inplace=True)
+    logger.info('\n{}\n'.format(rvs.info()))
 
     # Derivations
     derivations = hopkins.algorithms.derivations.Derivations()
 
-    usc = derivations.exc(blob=hencecounty, inhabitants=inhabitants)
+    usc = derivations.exc(blob=rvc, inhabitants=inhabitants)
     logger.info('\n{}\n'.format(usc.info()))
 
-    uss = derivations.exc(blob=hencestate, inhabitants=inhabitants)
+    uss = derivations.exc(blob=rvs, inhabitants=inhabitants)
     logger.info('\n{}\n'.format(uss.info()))
+
+    # Depositing
+    partitions = hopkins.algorithms.partitions. \
+        Partitions(blob=usc.drop(columns=[datestring, inhabitants]), partitionby='STUSPS')
+    partitions.exc(category='county', segment='baselines')
+
+    low = hopkins.algorithms.segments.Segments(blob=usc, category='county')
+    low.exc(select=['datetimeobject', 'epochmilli', 'STATEFP', 'COUNTYGEOID', 'positiveRate', 'deathRate'],
+            segment='capita')
+
+    high = hopkins.algorithms.segments.Segments(blob=uss, category='state')
+    high.exc(select=uss.columns.drop(labels=['date', 'STUSPS', 'POPESTIMATE2019']),
+             segment='baselines')
+    high.exc(select=['datetimeobject', 'epochmilli', 'STATEFP', 'positiveRate', 'deathRate'],
+             segment='capita')
+
+    # Spreads
+    logger.info(warehouse)
+    logger.info(os.path.join(warehouse, 'county', 'baselines'))
+    distributions = hopkins.spreads.distributions.Distributions(level='county', via='COUNTYGEOID')
+    numbers = distributions.exc(path=os.path.join(warehouse, 'county', 'baselines'))
+    logger.info('\n{}\n'.format(numbers))
 
 
 if __name__ == '__main__':
@@ -71,12 +95,18 @@ if __name__ == '__main__':
 
     import hopkins.algorithms.anomalies
     import hopkins.algorithms.derivations
+    import hopkins.algorithms.partitions
+    import hopkins.algorithms.segments
+
+
 
     # Create a config instance and empty the results storage directories
     configurations = config.Config()
     configurations.storage()
     days = configurations.days()
     inhabitants = configurations.inhabitants
+    datestring = configurations.datestring
+    warehouse = configurations.warehouse
 
     # Utilities
     import hopkins.base.utilities
@@ -94,5 +124,7 @@ if __name__ == '__main__':
     import populations.us.reference.county
 
     county = populations.us.reference.county.County()
+
+    import hopkins.spreads.distributions
 
     main()
