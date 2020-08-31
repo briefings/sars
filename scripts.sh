1deb8fe4b781aadcabfa8cfa55e7dbbbebb35462
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-08-31 19:15:29 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -3,9 +3,6 @@
 # Atlantic's COVID Tracking Project
 cp fundamentals/atlantic/warehouse/candles/*.json graphs/spreads/data/atlantic
 
-cp fundamentals/atlantic/warehouse/gazetteer.csv graphs/statistics/atlantic
-cp fundamentals/atlantic/warehouse/increases.csv graphs/statistics/atlantic
-
 # Hopkins
 cp -r fundamentals/hopkins/warehouse/county/candles/ graphs/spreads/data/hopkins/county/
 cp -r fundamentals/hopkins/warehouse/state/candles/ graphs/spreads/data/hopkins/state/
