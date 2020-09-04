b6c2d2f3bc39fde7b3d47e58fda1575aad60486a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-09-04 21:07:52 +0100

    Update fundamentals/atlantic/atlantic/spreads/attributes.py

--- fundamentals/atlantic/atlantic/spreads/attributes.py
+++ fundamentals/atlantic/atlantic/spreads/attributes.py
@@ -25,24 +25,30 @@ class Attributes:
 
     @staticmethod
     def variables():
-        return ['positiveIncrease', 'testIncrease', 'deathIncrease', 'positiveCumulative', 'testCumulative',
-                'deathCumulative', 'positiveIncreaseRate', 'testIncreaseRate', 'deathIncreaseRate',
-                'positiveRate', 'testRate', 'deathRate']
+        return ['positiveIncrease', 'testIncrease', 'deathIncrease', 'hospitalizedIncrease',
+                'positiveCumulative', 'testCumulative', 'deathCumulative', 'hospitalizedCumulative',
+                'positiveIncreaseRate', 'testIncreaseRate', 'deathIncreaseRate', 'hospitalizedIncreaseRate',
+                'positiveRate', 'testRate', 'deathRate', 'hospitalizedRate']
 
     @staticmethod
     def fields():
-        return ['datetimeobject', 'STUSPS', 'positiveIncrease', 'testIncrease', 'deathIncrease',
-                'positiveCumulative', 'testCumulative', 'deathCumulative',
-                'positiveIncreaseRate', 'testIncreaseRate', 'deathIncreaseRate',
-                'positiveRate', 'testRate', 'deathRate', 'ndays']
+        return ['datetimeobject', 'STUSPS',
+                'positiveIncrease', 'testIncrease', 'deathIncrease', 'hospitalizedIncrease',
+                'positiveCumulative', 'testCumulative', 'deathCumulative', 'hospitalizedCumulative',
+                'positiveIncreaseRate', 'testIncreaseRate', 'deathIncreaseRate', 'hospitalizedIncreaseRate',
+                'positiveRate', 'testRate', 'deathRate', 'hospitalizedRate', 'ndays']
 
     @staticmethod
     def dtype():
-        return {'STUSPS': 'str', 'positiveIncrease': np.float64, 'testIncrease': np.float64,
-                'deathIncrease': np.float64, 'positiveCumulative': np.float64, 'testCumulative': np.float64,
-                'deathCumulative': np.float64, 'positiveIncreaseRate': np.float64, 'testIncreaseRate': np.float64,
-                'deathIncreaseRate': np.float64, 'positiveRate': np.float64, 'testRate': np.float64,
-                'deathRate': np.float64, 'ndays': np.int64}
+        return {'STUSPS': 'str',
+                'positiveIncrease': np.float64, 'testIncrease': np.float64,
+                'deathIncrease': np.float64, 'hospitalizedIncrease': np.float,
+                'positiveCumulative': np.float64, 'testCumulative': np.float64,
+                'deathCumulative': np.float64, 'hospitalizedCumulative': np.float,
+                'positiveIncreaseRate': np.float64, 'testIncreaseRate': np.float64,
+                'deathIncreaseRate': np.float64, 'hospitalizedIncreaseRate': np.float,
+                'positiveRate': np.float64, 'testRate': np.float64,
+                'deathRate': np.float64, 'hospitalizedRate': np.float, 'ndays': np.int64}
 
     @staticmethod
     def parse_dates():
