5b507d64a2d8b809e16d7e5149e763502298bc36
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-09-05 18:20:00 +0100

    Update fundamentals/hopkins/hopkins/spreads/attributes.py

--- fundamentals/hopkins/hopkins/spreads/attributes.py
+++ fundamentals/hopkins/hopkins/spreads/attributes.py
@@ -3,13 +3,9 @@ import numpy as np
 
 class Attributes:
 
-    def __init__(self, level: str):
-
+    def __init__(self):
         # The candle, i.e., quantile, points of interest
         self.points = np.array((0.1, 0.25, 0.5, 0.75, 0.9))
-
-        # county or state
-        self.level = level
 
     @staticmethod
     def variables():
@@ -17,27 +13,17 @@ class Attributes:
                 'deathCumulative', 'positiveIncreaseRate', 'deathIncreaseRate',
                 'positiveRate', 'deathRate']
 
-    def fields(self):
+    @staticmethod
+    def fields():
+        return ['epochmilli', 'STUSPS', 'COUNTYGEOID', 'positiveIncrease', 'deathIncrease',
+                'positiveCumulative', 'deathCumulative',
+                'positiveIncreaseRate', 'deathIncreaseRate',
+                'positiveRate', 'deathRate', 'ndays']
 
-        fv = ['epochmilli', 'STUSPS', 'positiveIncrease', 'deathIncrease',
-              'positiveCumulative', 'deathCumulative',
-              'positiveIncreaseRate', 'deathIncreaseRate',
-              'positiveRate', 'deathRate', 'ndays']
-
-        if self.level == 'county':
-            fv.append('COUNTYGEOID')
-
-        return fv
-
-    def dtype(self):
-
-        dv = {'epochmilli': np.longlong, 'STUSPS': 'str',
-              'positiveIncrease': np.float64, 'deathIncrease': np.float64,
-              'positiveCumulative': np.float64, 'deathCumulative': np.float64,
-              'positiveIncreaseRate': np.float64, 'deathIncreaseRate': np.float64,
-              'positiveRate': np.float64, 'deathRate': np.float64, 'ndays': np.int64}
-
-        if self.level == 'county':
-            dv['COUNTYGEOID'] = 'str'
-
-        return dv
+    @staticmethod
+    def dtype():
+        return {'epochmilli': np.longlong, 'STUSPS': 'str', 'COUNTYGEOID': 'str',
+                'positiveIncrease': np.float64, 'deathIncrease': np.float64,
+                'positiveCumulative': np.float64, 'deathCumulative': np.float64,
+                'positiveIncreaseRate': np.float64, 'deathIncreaseRate': np.float64,
+                'positiveRate': np.float64, 'deathRate': np.float64, 'ndays': np.int64}
