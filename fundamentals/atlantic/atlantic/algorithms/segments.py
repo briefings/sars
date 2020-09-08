b541cc9c5a4e42277d9446e194b49e110f1129e6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-09-08 16:16:57 +0100

    Update fundamentals/atlantic/atlantic/algorithms/segments.py: Minimal re-writes, and cleaning-up

--- fundamentals/atlantic/atlantic/algorithms/segments.py
+++ fundamentals/atlantic/atlantic/algorithms/segments.py
@@ -20,35 +20,26 @@ class Segments:
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
 
-    def increases(self):
-        data = self.blob[['datetimeobject', 'STUSPS', 'positiveIncrease', 'testIncrease',
-                          'deathIncrease', 'hospitalizedIncrease']].copy()
-
-        data.to_csv(path_or_buf=os.path.join(self.warehouse, 'increases.csv'),
-                    header=True, index=False, encoding='utf-8')
-
     def baselines(self):
         data = self.blob.copy()
         data = data.drop(columns='POPESTIMATE2019', inplace=False)
 
-        self.logger.info('\nBaselines\n{}\n'.format(data.info()))
+        self.logger.info('\nBaselines:\n{}\n'.format(data.info()))
         data.to_csv(path_or_buf=os.path.join(self.warehouse, 'baselines.csv'),
                     header=True, index=False, encoding='utf-8')
 
     def capita(self):
 
         data = self.blob.copy()
-        data = data.drop(
-            columns=['POPESTIMATE2019', 'positiveIncrease', 'testIncrease', 'deathIncrease', 'hospitalizedIncrease',
-                     'positiveCumulative', 'testCumulative', 'deathCumulative', 'hospitalizedCumulative',
-                     'positiveIncreaseRate', 'testIncreaseRate', 'deathIncreaseRate', 'hospitalizedIncreaseRate'])
+        data = data[['datetimeobject', 'STUSPS', 'deathRate', 'positiveRate', 'testRate',
+                     'icuRate', 'hospitalizedRate', 'ndays']]
 
         gridlines = atlantic.algorithms.gridlines.GridLines(positive_rate_max=data['positiveRate'].max(),
                                                             test_rate_max=data['testRate'].max()).exc()
 
         data = pd.concat([data, gridlines], axis=0, ignore_index=True)
 
-        self.logger.info('\nCapita\n{}\n'.format(data.info()))
+        self.logger.info('\nCapita:\n{}\n'.format(data.info()))
         data.to_csv(path_or_buf=os.path.join(self.warehouse, 'capita.csv'),
                     header=True, index=False, encoding='utf-8')
         data.to_json(path_or_buf=os.path.join(self.warehouse, 'capita.json'), orient='values')
