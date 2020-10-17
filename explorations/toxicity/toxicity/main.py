b3c6043055985d245cc1bd747e696f219bce7dd2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-10-17 01:35:55 +0100

    Update explorations/toxicity/toxicity/main.py

--- explorations/toxicity/toxicity/main.py
+++ explorations/toxicity/toxicity/main.py
@@ -2,19 +2,43 @@ import logging
 import os
 import sys
 
+import glob
+
 
 def main():
     # Logging
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    gazetteer = references.gazetteer()
-    logger.info('\n{}\n'.format(gazetteer.tail()))
-
+    # Directories
     directories.cleanup(listof=configurations.paths)
     directories.create(listof=configurations.paths)
 
+    # For ...
     intersections.exc()
+
+    # Subsequently ...
+
+    # Gazetter
+    gazetteer = references.gazetteer()
+    logger.info('\n{}\n'.format(gazetteer.tail()))
+
+    # Partitions instance
+    partitions = toxicity.algorithms.partitions.Partitions(gazetteer=gazetteer)
+
+    sources = glob.glob(os.path.join(configurations.warehouse, 'modelling', '*.csv'))
+    for source in sources:
+
+        case, classification = configurations.names(source=source)
+
+        directory = os.path.join(configurations.warehouse, 'graphing', case, classification)
+        directories.create(listof=[directory])
+
+        print(directory, '\n', source)
+
+        partitions.exc(source=source, directory=directory)
+
+
 
 
 if __name__ == '__main__':
@@ -26,11 +50,13 @@ if __name__ == '__main__':
     import toxicity.src.references
     import toxicity.base.directories
     import toxicity.algorithms.intersections
+    import toxicity.algorithms.partitions
 
     # Instances
     configurations = config.Config()
     references = toxicity.src.references.References()
     directories = toxicity.base.directories.Directories()
     intersections = toxicity.algorithms.intersections.Intersections()
+
 
     main()
