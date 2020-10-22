2c2eca7142a9b39152339fd87abe4adf7087805c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-10-22 19:03:47 +0100

    Add fundamentals/hopkins/apply.sh

---
+++ fundamentals/hopkins/apply.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git commit fundamentals/hopkins/notebooks/warehouse/states/candles/ -m "Latest fundamentals/hopkins/notebooks/warehouse/states/candles/"
+git commit explorations/toxicity/warehouse/modelling/ -m "Latest explorations/toxicity/warehouse/modelling/"
+git commit explorations/toxicity/warehouse/graphing/immunological/ -m "Latest explorations/toxicity/warehouse/graphing/immunological/"
