8e20c9acb904b0235a8c7b6e3c84f192db3f1d51
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-10-22 19:39:41 +0100

    Add explorations/toxicity/apply.sh

---
+++ explorations/toxicity/apply.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git commit explorations/toxicity/warehouse/graphing/immunological/group/ -m "Latest explorations/toxicity/warehouse/graphing/immunological/group/"
+git commit explorations/toxicity/warehouse/graphing/immunological/pollutant/ -m "Latest explorations/toxicity/warehouse/graphing/immunological/pollutant/"
+git commit explorations/toxicity/warehouse/graphing/respiratory/group/ -m "Latest explorations/toxicity/warehouse/graphing/respiratory/group/"
+git commit explorations/toxicity/warehouse/graphing/respiratory/pollutant/ -m "Latest explorations/toxicity/warehouse/graphing/respiratory/pollutant/"
+
+git commit explorations/toxicity/warehouse/modelling/ -m "Latest explorations/toxicity/warehouse/modelling/"
