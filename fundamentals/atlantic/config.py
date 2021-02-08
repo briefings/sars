857cb656fd3b8667e4d62d57bcda838aef55c7e8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-08 08:52:26 +0000

    Update fundamentals/atlantic/config.py: Cleaning-up

--- fundamentals/atlantic/config.py
+++ fundamentals/atlantic/config.py
@@ -16,7 +16,7 @@ class Config:
         # https://covidtracking.com/data/api
         self.starting: str = '2020-03-09'
         self.epochdays: int = int(datetime.strptime(self.starting, '%Y-%m-%d').timestamp() / (60 * 60 * 24))
-        self.ending: str = (datetime.today() - timedelta(days=2)).strftime('%Y-%m-%d')
+        self.ending: str = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
         self.datestring = 'date'
         self.datepattern = '%Y%m%d'
 
