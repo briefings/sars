0d01c03c21e4851b7ac4fa8748c22485dcdf5b93
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-08 08:52:09 +0000

    Update fundamentals/atlantic/atlantic/main.py: Cleaning-up

--- fundamentals/atlantic/atlantic/main.py
+++ fundamentals/atlantic/atlantic/main.py
@@ -31,9 +31,6 @@ def main():
     # Addressing Anomalies
     anomalies = atlantic.algorithms.anomalies.Anomalies(blob=readings).exc()
 
-    # Tests
-    # tests = atlantic.algorithms.tests.Tests(blob=anomalies).exc()
-
     # Derivations
     derive = atlantic.algorithms.derivations.Derivations(data=anomalies)
     derivations = derive.exc(places=anomalies['STUSPS'].unique())
@@ -65,7 +62,6 @@ if __name__ == '__main__':
     import config
 
     import atlantic.algorithms.anomalies
-    import atlantic.algorithms.tests
     import atlantic.algorithms.derivations
     import atlantic.algorithms.accumulations
     import atlantic.algorithms.segments
@@ -83,19 +79,16 @@ if __name__ == '__main__':
 
     # Utilities
     import atlantic.base.utilities
-
     atlantic.base.utilities.Utilities().exc()
 
     # Utilities: Cartographs
     import cartographs.boundaries.us.boundaries
     import cartographs.boundaries.us.settings
-
     settings = cartographs.boundaries.us.settings.Settings()
     boundaries = cartographs.boundaries.us.boundaries.Boundaries(crs=settings.crs)
 
     # Utilities: Populations
     import populations.us.reference.state
-
     state = populations.us.reference.state.State()
 
     # Utilities: Candles
